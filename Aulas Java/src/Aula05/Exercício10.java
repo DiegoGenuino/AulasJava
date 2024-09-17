e8fa6d97063d17aaf9690c10321e893aab145cac
DiegoGenuino <genuinodiego11@gmail.com>
2024-09-17 16:05:53 -0300

    Código alterado.

--- Aulas Java/src/Aula05/Exercício10.java
+++ Aulas Java/src/Aula05/Exercício10.java
@@ -3,27 +3,27 @@ package Aula05;
 public class Exercício10 {
     public static void main(String[] args) {
         float valorReais = 134f;
-        int codigoMoeda = 1;
+        int codigoMoeda = 3;
         float valorConvertido;
 
         switch (codigoMoeda) {
             case 1:
-                valorConvertido = valorReais * 5.50f;
-                System.out.println("Valor convertido: " + valorConvertido + " Dólares");
+                valorConvertido = valorReais / 5.50f;
+                System.out.printf("Valor convertido: %.2f USD", valorConvertido);
                 break;
 
             case 2:
-                valorConvertido = valorReais * 6.11f;
-                System.out.println("Valor convertido: " + valorConvertido + " Euros");
+                valorConvertido = valorReais / 6.11f;
+                System.out.printf("Valor convertido: %.2f EUR", valorConvertido);
                 break;
 
             case 3:
-                valorConvertido = valorReais * 7.23f;
-                System.out.println("Valor convertido: " + valorConvertido + " Libras");
+                valorConvertido = valorReais / 7.23f;
+                System.out.printf("Valor convertido: %.2f GBP", valorConvertido);
                 break;
 
             default:
-                System.out.println("Código da moeda inválido.");
+                System.out.printf("Código da moeda inválido.");
         }
     }
 }
